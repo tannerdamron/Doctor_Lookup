72d5c5ab5f8e7db10c610dc45bd0ef7ff17dbc20
Tanner Damron <tanner.mdamron@yahoo.com>
2019-03-29 10:43:34 -0700

    fix api key call, and return tiny piece of information to make sure method is working

--- src/js/doctorLookup.js
+++ src/js/doctorLookup.js
@@ -2,12 +2,11 @@ export default class userSearch {
   constructor(ailment, doctorName) {
     this.ailment = ailment;
     this.doctorName = doctorName;
-    this.apiKey = process.env.exports.apiKey;
-    this.ailmentUrl = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.ailment}&location=or-portland&limit=50&user_key=${this.apiKey}`;
-    this.nameUrl = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.doctorName}&location=or-portlandlimit=50&user_key=${this.apiKey}`;
+    this.uniqueApiKey = process.env.apiKey;
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.doctorName}&query=${this.ailment}&location=or-portland&limit=50&user_key=${this.uniqueApiKey}`;
   }
 
-  doctorSearchByName() {
+  doctorSearch() {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       request.onload = function () {
@@ -17,23 +16,23 @@ export default class userSearch {
           reject(Error(request.statusText));
         }
       };
-      request.open("GET", this.nameUrl, true);
+      request.open("GET", this.url, true);
       request.send();
     });
   }
 
-  doctorSearchByAilment() {
-    return new Promise((resolve, reject) => {
-      let request = new XMLHttpRequest();
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      };
-      request.open("GET", this.ailmentUrl, true);
-      request.send();
-    });
-  }
+  // doctorSearchByAilment() {
+  //   return new Promise((resolve, reject) => {
+  //     let request = new XMLHttpRequest();
+  //     request.onload = function () {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(Error(request.statusText));
+  //       }
+  //     };
+  //     request.open("GET", this.ailmentUrl, true);
+  //     request.send();
+  //   });
+  //}
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import userSearch from './doctorLookup';
+import userSearch from './js/doctorLookup';
 import $ from 'jquery';
 
 $(document).ready(function () {
   let ailment = $('#ailment').val();
   let doctorName = $('#doctorName').val();
   let userInput = new userSearch(ailment, doctorName);
-  let promise = userInput.doctorSearchByName();
+  let promise = userInput.doctorSearch();
+
   promise.then(function(response) {
     let body = JSON.parse(response);
-    $('#results').text(`${body.name}`);
+    $('#results').text(`${body.data[0].profile.first_name}`);
   }, function (error) {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
