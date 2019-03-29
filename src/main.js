57d4201749e0870ddbeec315e7aebf8cc486a210
Tanner Damron <tanner.mdamron@yahoo.com>
2019-03-29 12:25:30 -0700

    add table and refine returns for table use

--- src/main.js
+++ src/main.js
@@ -5,31 +5,40 @@ import './styles.css';
 $(document).ready(function () {
   $('#userSearchForm').submit(function(e) {
     e.preventDefault();
+    $('#noResults').hide();
     let ailment = $('#ailment').val();
     let doctorName = $('#doctorName').val();
+    let newPatients = "";
+    let website = "";
     let userInput = new userSearch(ailment, doctorName);
     let promise = userInput.doctorSearch();
     promise.then(function (response) {
       let body = JSON.parse(response);
-      console.log(body.data.length);
       if (body.data.length === 0) {
-        $('.showErrors').text("Your search returned no results");
+        $('#noResults').text("Your search returned no results").show();
         $('#results').hide();
       }
+      for (let i = 0; i < body.data.length; i++) {
+        $("#table").append(`<tr id=${i}></tr>`);
+      }
       for (let i = 0; i <= body.data.length; i++) {
-        $('#resultDoctorName').text(`${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`);
-        $('#resultDoctorAddress').text(`${body.data[i].practices[0].visit_address.street}. ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state_long}`);
-        $('#resultDoctorPhoneNumber').text(`${body.data[i].practices[0].phones[0].number}`);
-        if (`${body.data[i].practices[0].website}` === "undefined") {
-          $('#resultDoctorWebsite').text("This doctor does not have a website");
+        if (body.data[i].practices[0].accepts_new_patients === true) {
+          newPatients = "Yes";
         } else {
-          $('#resultDoctorWebsite').text(`${body.data[i].practices[0].website}`);
+          newPatients = "No";
         }
-        if (`${body.data[i].practices[0].accepts_new_patients}` === true) {
-          $('#resultDoctorNewPatients').text("Yes");
+        if (body.data[i].practices[0].website === undefined) {
+          website = "No website";
         } else {
-          $('#resultDoctorNewPatients').text("No");
+          website = body.data[i].practices[0].website;
         }
+        $('#' + i).html(`
+        <td>${body.data[i].profile.first_name}</td>
+        <td>${body.data[i].profile.last_name}</td>
+        <td> ${body.data[i].practices[0].visit_address.street}. ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state_long}</td>
+        <td>${body.data[i].practices[0].phones[0].number}</td>
+        <td>${website}</td>
+        <td>${newPatients}</td>`);
       }
     }, function (error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
