90baae61b978f91256a6c6e6c9cd260fa43c5576
Tanner Damron <tanner.mdamron@yahoo.com>
2019-03-29 11:44:33 -0700

    fix no results return

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import './styles.css';
 
 $(document).ready(function () {
-  $('#userSearchForm').submit(function (e) {
+  $('#userSearchForm').submit(function(e) {
     e.preventDefault();
     let ailment = $('#ailment').val();
     let doctorName = $('#doctorName').val();
@@ -14,26 +14,26 @@ $(document).ready(function () {
       console.log(body.data.length);
       if (body.data.length === 0) {
         $('.showErrors').text("Your search returned no results");
-      } else {
-        for (let i = 0; i <= body.data.length; i++) {
-          $('#resultDoctorName').text(`${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`);
-          $('#resultDoctorAddress').text(`${body.data[i].practices[0].visit_address.street}. ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state_long}`);
-          $('#resultDoctorPhoneNumber').text(`${body.data[i].practices[0].phones[0].number}`);
-          if (`${body.data[i].practices[0].website}` === "undefined") {
-            $('#resultDoctorWebsite').text("This doctor does not have a website");
-          } else {
-            $('#resultDoctorWebsite').text(`${body.data[i].practices[0].website}`);
-          }
-          if (`${body.data[i].practices[0].accepts_new_patients}` === true) {
-            $('#resultDoctorNewPatients').text("Yes");
-          } else {
-            $('#resultDoctorNewPatients').text("No");
-          }
+        $('#results').hide();
+      }
+      for (let i = 0; i <= body.data.length; i++) {
+        $('#resultDoctorName').text(`${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`);
+        $('#resultDoctorAddress').text(`${body.data[i].practices[0].visit_address.street}. ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state_long}`);
+        $('#resultDoctorPhoneNumber').text(`${body.data[i].practices[0].phones[0].number}`);
+        if (`${body.data[i].practices[0].website}` === "undefined") {
+          $('#resultDoctorWebsite').text("This doctor does not have a website");
+        } else {
+          $('#resultDoctorWebsite').text(`${body.data[i].practices[0].website}`);
         }
-        $('#results').show();
+        if (`${body.data[i].practices[0].accepts_new_patients}` === true) {
+          $('#resultDoctorNewPatients').text("Yes");
+        } else {
+          $('#resultDoctorNewPatients').text("No");
+        }
       }
     }, function (error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
+    $('#results').show();
   });
 });
